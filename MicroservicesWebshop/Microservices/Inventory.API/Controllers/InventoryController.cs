f457cc019bfa5ecb059fb9684cc5fbcfe58e030c
iulian-b97 <iulian.burghelea1997@gmail.com>
2022-11-27 20:37:29 +0200

    Create solution

---
+++ MicroservicesWebshop/Microservices/Inventory.API/Controllers/InventoryController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Inventory.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InventoryController : ControllerBase
+    {
+    }
+}
