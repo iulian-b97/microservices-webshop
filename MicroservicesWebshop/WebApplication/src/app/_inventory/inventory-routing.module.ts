254b19f41e1cf0f5e27f208ea03d84a7d366513c
iulian-b97 <iulian.burghelea1997@gmail.com>
2023-01-06 15:47:16 +0200

    Create Inventory UI & Implement authentication guard

--- MicroservicesWebshop/WebApplication/src/app/_inventory/inventory-routing.module.ts
+++ MicroservicesWebshop/WebApplication/src/app/_inventory/inventory-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from '../auth/auth.guard';
 
 import { InventoryComponent } from './inventory.component';
 
 const routes: Routes = [
-  { path: 'inventory', component: InventoryComponent }
+  { path: 'inventory', component: InventoryComponent, canActivate:[AuthGuard] }
 ];
 
 @NgModule({
